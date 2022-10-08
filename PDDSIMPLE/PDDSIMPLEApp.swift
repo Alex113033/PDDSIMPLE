0ac71cab7e4ef7992958d7f6042bc29ddf44ebe1
Alexander Gnilitskiy <gnilitskiy1997@icloud.com>
2022-10-08 18:43:41 +0300

    Initial Commit

---
+++ PDDSIMPLE/PDDSIMPLEApp.swift
@@ -0,0 +1,17 @@
+//
+//  PDDSIMPLEApp.swift
+//  PDDSIMPLE
+//
+//  Created by Александр on 08.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct PDDSIMPLEApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
